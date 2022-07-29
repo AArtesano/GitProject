cd7ef75fbd9d17b0092f4d5426c726fe99afe515
RobieAnnPiojo <robieannpiojo@gmail.com>
2022-07-29 12:02:10 +0800

    controller and model folders

---
+++ GitProject/Model/Guardian_Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitProject.Model
+{
+    public class Guardian_Model
+    {
+        public int GuardianID { get; set; }
+        public int StudentID { get; set; }
+        public string Firstname { get; set; }
+        public string Middlename { get; set; }
+        public string Lastname { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Relationship { get; set; }
+    }
+}
